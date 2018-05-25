817d18a791277c0fa9f183eedd7da8f9ed727f80
Vinicius dos Santos <vinicius6.santos@usp.br>
2018-05-25 11:25:43 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# gcplot
-Automated plotting software for growth curves with biological replicates
+<p align="center"><img src="misc/logo.png" alt="GCPlot" width="300"></p>
+
+GCPlot is a plotting software for growth curves with biological replicates. It works with experiments as growing cells under different conditions or growing different cells under the same conditions. Each of this _differable_ varbiable, here, is called **treatment**. So, if one wants to compare growth of cells of the **specie A** under **37°C** and **40°C**, it's said that there are two different treatments: 37°C and 40°C. It is the same if one wants to compare growth of cells of **specie A** and **specie B** under a fixed condition of 37°C; there are two different treatments: specie A and specie B.
+
+## Requirement
+GCPlot only requires `R` and the package `ggplot2`
+
+## Usage
+
+```sh
+$ R -f GCPlot.R --args <growth_file.txt> <spec_file.spec> <plot_name.png>
+```
+* **growth_file.txt** - A file containing the growth data tabular separated. Each row must have the growth estimation on each time, and the columns must correspond to the replicates.
+
+ The example bellow shows a file with columns corresponding to 2 different treatments with 3 replicates each;
+ 
+ |time|treatment1 - replicate1|treatment1 - replicate2|treatment2 - replicate1|treatment2 - replicate2|
+ |---:|---:|---:|---:|---:|
+ |0|0.5|0.5|0.5|0.5|
+ |1|0.6|0.6|0.5|0.5|
+ |...|...|...|...|...|
+ |180|2.1|2.4|0.7|0.9|
+ 
+ This treatments can be either different strains of cells or even different environmental contitions. In the second case, it would look like follows:
+ 
+ |time|condition1 - replicate1|condition1 - replicate2|condition2 - replicate1|condition2 - replicate2|
+ |---:|---:|---:|---:|---:|
+ |0|0.5|0.5|0.5|0.5|
+ |1|0.6|0.6|0.5|0.5|
+ |...|...|...|...|...|
+ |180|2.1|2.4|0.7|0.9|
+ 
+ For example, see `gc_example_file.txt`
+ 
+* **spec_file.spec** A file containing information about the experiment. The lines must contain, basically, information to   fill the output chart. The first two lines coresponds to X and Y label, respectively, and last one to the chart title.
+ This file must be like follows:
+ 
+ | |
+ |-|
+ |Growth estimation method (OD, NCels..), **default = `OD600`**|
+ |Time units (hours, minutes ..), **default = `hours`**|
+ |Chart title, **default = `NULL`**|
+ 
+ For example, see `spec_example_file.spec`
+ 
+ * **plot_name.png** Filename of the chart figure. It can be `png`, `svg`, `jpg` and all other formats supported by ggplot.
+
+
+
+
+
---
+++ gcplot.R
@@ -0,0 +1,93 @@
+
+##########################
+# REPLICATE GROWTH CURVE #
+##########################
+
+# Processing arguments
+args = commandArgs(trailingOnly = T)
+if((length(args) == 0) || args[1] == "help") {
+	stop("No files were suplied. Exiting...
+\n
+Usage:
+======
+\n
+	R -f GCPlot.R --args <growth_file.txt> <spec_file.spec> <plot_name.png>
+\n
+Please, make sure the R package ggplot2 is installed successfully
+If you are heaving trouble dealing with the file types, please, see README.md
+\n
+\n
+GCPlot: An automated pipeline for plotting growth curves
+=========================================================
+by Vinícius H F dos Santos
+Version 1.0
+2018
+\n
+")
+}
+# Loading packages
+library(ggplot2)
+
+# Loading files
+gc_header = read.table(args[1], stringsAsFactors = F)
+spec = read.table(args[2], stringsAsFactors = F)
+flname = paste0(args[3])
+
+# Getting treatment information
+treat = unique(as.character(gc_header[1,2:ncol(gc_header)]))
+nrep = length(which(gc_header[1,] == treat[1]))
+
+
+if(is.na(spec[3,1])){ titl = " " }else{ titl = paste(strsplit(as.character(spec[3,1]), split = "_")[[1]], collapse = " ")}
+if(is.na(spec[2,1])){ ylab = "OD600" }else{ ylab = as.character(spec[2,1]) }
+if(is.na(spec[1,1])){ xlab = "Time (hours)" }else{ xlab = paste0("Time (", spec[1,1], ")") }
+
+# GC: Time | treat_replicate1 | treat_replicate2 | treat_replicate1 | ....... | treatn_replicaten
+gc = as.data.frame(gc_header[2:nrow(gc_header),])
+cols = ncol(gc)
+rows = nrow(gc)
+colnames(gc) = 1:cols
+rownames(gc) = 1:rows
+
+nspp = (cols-1)/nrep
+df.list = list()
+
+# Creating splitted dataframes
+for(i in 1:nspp){
+	if(i == 1){from = 2}
+	to = from+nrep-1 # 4
+	growth_mean = apply(data.matrix(gc[,from:to]), 1, mean)
+	growth_sd = apply(data.matrix(gc[,from:to]), 1, sd)
+	data = data.frame(Time = as.numeric(gc[,1]), 
+			  Mean = growth_mean,
+			  sd = growth_sd,
+			  Treatment = treat[i])
+	df.list[[i]] = data
+	from = to+1
+}
+
+# Meging them
+df.merged = do.call(rbind, df.list)
+
+# ggplotting
+p = ggplot(data = df.merged, aes(x = Time, y = Mean)) +
+	geom_line(aes(colour = Treatment), size = 1.3) +
+	geom_errorbar(aes(ymin = Mean-sd, ymax = sd+Mean, colour = Treatment), size = 0.2) +
+	geom_point(aes(colour = Treatment), size = 1.4)+
+	labs(title = titl, x = xlab, y = ylab) + 
+	theme_classic() +
+	theme(text = element_text(size = 20),
+		plot.title = element_text(hjust = 0.5),
+		legend.title=element_blank())
+
+ggsave(filename = flname, plot = p)
+
+# Exitting...
+
+
+# GCPlot: An automated pipeline for plotting growth curves
+# =========================================================
+#
+# by Vinícius H F dos Santos
+# Version 1.0
+# 2018
